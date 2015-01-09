02f7d0d2242c024cd9520d2a09ff61d6fbc3fe0f
Terry Stewart <tcstewar@uwaterloo.ca>
2015-01-09 01:02:54 -0500

    Handled slider overrides

--- nengo_viz.py
+++ nengo_viz.py
@@ -12,14 +12,12 @@ class Server(swi.SimpleWebInterface):
         return html % dict(components=components)
 
     def ws_viz_component(self, client, id):
-        print 'viz', client, id
         component = self.viz.get_component(int(id))
-        print 'component', component
 
         while True:
             msg = client.read()
             while msg is not None:
-                print id, msg
+                component.message(msg)
                 msg = client.read()
 
             component.update_client(client)
@@ -39,12 +37,28 @@ class Component(object):
         self.height = height
     def update_client(self, client):
         pass
+    def message(self, msg):
+        print 'unhandled message', msg
 
 class Slider(Component):
     def __init__(self, viz, node, **kwargs):
         super(Slider, self).__init__(viz, **kwargs)
         self.node = node
-        self.override = np.zeros(node.size_out)
+        self.base_output = node.output
+        node.output = self.override_output
+        self.override = [None] * node.size_out
+        self.value = np.zeros(node.size_out)
+
+    def override_output(self, t, *args):
+        if callable(self.base_output):
+            self.value[:] = self.base_output(t, *args)
+        else:
+            self.value[:] = self.base_output
+
+        for i, v in enumerate(self.override):
+            if v is not None:
+                self.value[i] = v
+        return self.value
 
     def javascript(self):
         return ('new VIZ.Slider({parent:main, n_sliders:%(n_sliders)d, '
@@ -52,6 +66,13 @@ class Slider(Component):
                 'width:%(width)g, height:%(height)g, id:%(id)d});' %
                 dict(x=self.x, y=self.y, width=self.width, height=self.height,
                  n_sliders=len(self.override), id=id(self)))
+
+    def message(self, msg):
+        index, value = msg.split(',')
+        index = int(index)
+        value = float(value)
+        self.override[index] = value
+
 
 class Value(Component):
     def __init__(self, viz, obj, **kwargs):
