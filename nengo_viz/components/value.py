faae5de82bd8d3a9bfcf768b3bf45a915d9750d7
Terry Stewart <tcstewar@uwaterloo.ca>
2015-02-11 16:44:22 -0800

    Added display_time option to value graph

--- nengo_viz/components/value.py
+++ nengo_viz/components/value.py
@@ -6,11 +6,12 @@ from nengo_viz.components.component import Component
 
 
 class Value(Component):
-    def __init__(self, viz, obj, **kwargs):
+    def __init__(self, viz, obj, display_time=True, **kwargs):
         super(Value, self).__init__(viz, **kwargs)
         self.obj = obj
         self.data = []
         self.n_lines = obj.size_out
+        self.display_time = display_time
         self.struct = struct.Struct('<%df' % (1 + self.n_lines))
         with viz.model:
             self.node = nengo.Node(self.gather_data, size_in=obj.size_out)
@@ -32,6 +33,7 @@ class Value(Component):
         return ('new VIZ.Value({parent:main, sim:sim, '
                 'x:%(x)g, y:%(y)g, '
                 'width:%(width)g, height:%(height)g, id:%(id)d, '
-                'n_lines:%(n_lines)d});' %
+                'n_lines:%(n_lines)d, display_time:%(display_time)s});' %
                 dict(x=self.x, y=self.y, width=self.width, height=self.height,
-                     id=id(self), n_lines=self.n_lines))
+                     id=id(self), n_lines=self.n_lines,
+                     display_time = 'true' if self.display_time else 'false'))
