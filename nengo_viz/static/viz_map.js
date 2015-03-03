d8e43cdcfea1e3a459f93ef41181f1ae1399240e
Chris Chan <c88chan@CTN05.(none)>
2015-03-03 15:57:23 -0500

    edits

--- nengo_viz/static/viz_map.js
+++ nengo_viz/static/viz_map.js
@@ -1,19 +1,23 @@
 
+var map_on = true;
+
 VIZ.draw_map = function (){
     console.log("drawing");
     $('.minimap').remove();
     $('.miniregion').remove();
-    VIZ.map = $('body').minimap({
-        heightRatio : 0.2,
-        widthRatio : 0.2,
-        offsetHeightRatio : 0.035,
-        offsetWidthRatio : 0.035,
-        position : "right",
-        touch: true,
-        smoothScroll: true,
-        smoothScrollDelay: 200,
-        onPreviewChange: function() {}
-    });
+    if (map_on){
+        VIZ.map = $('body').minimap({
+            heightRatio : 0.2,
+            widthRatio : 0.2,
+            offsetHeightRatio : 0.70,
+            offsetWidthRatio : 0.035,
+            position : "right",
+            touch: true,
+            smoothScroll: true,
+            smoothScrollDelay: 200,
+            onPreviewChange: function() {}
+        });
+    }
 }
 VIZ.draw_map();
-setInterval(function(){VIZ.draw_map()}, 100);
+setInterval(function(){VIZ.draw_map()}, 1000);
