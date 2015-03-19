03eedcdb65233008347a68e681fde5142140dac0
Chris Chan <c88chan@CTN05.(none)>
2015-03-19 15:00:26 -0400

    rewrote window functions

--- nengo_viz/static/viz_window.js
+++ nengo_viz/static/viz_window.js
@@ -1,21 +1,17 @@
 /*This detects if the mouse has exited the browser window to prevent erratic behaviour upon re-entry*/
-
-function addEvent(obj, evt, fn) {
-    if (obj.addEventListener) {
-        obj.addEventListener(evt, fn, false);
+window.addEventListener('load',
+    function(e){
+        var main = document.getElementById('main');
+        main.addEventListener('mouseout',
+            function(e) {
+                if (!(e)) {
+                    e = window.event;
+                }
+                var from = e.relatedTarget || e.toElement;
+                if (!from || from.nodeName == "HTML") {
+                    $(main).trigger('mouseup'); // When the mouse leaves the #main element, trigger the mouseup event
+                }
+            }
+            )
     }
-    else if (obj.attachEvent) {
-        obj.attachEvent("on" + evt, fn);
-    }
-}
-
-var main = document.getElementById('main');
-addEvent(window,"load",function(e) {
-    addEvent(main, "mouseout", function(e) {
-        e = e ? e : window.event;
-        var from = e.relatedTarget || e.toElement;
-        if (!from || from.nodeName == "HTML") {
-            $(main).trigger('mouseup');
-        }
-    });
-});
+);
