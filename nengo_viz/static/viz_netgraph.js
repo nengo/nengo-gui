2a51fb4676c9021b27a13728b1b1ce5186738bd9
Terry Stewart <tcstewar@uwaterloo.ca>
2015-03-25 14:14:28 -0400

    Added documentation

--- nengo_viz/static/viz_netgraph.js
+++ nengo_viz/static/viz_netgraph.js
@@ -7,10 +7,8 @@
  * @param {DOMElement} args.parent - the element to add this component to
  */
 VIZ.NetGraph = function(args) {
-    var self = this;
 
-    n = this;
-
+    /** create the master SVG element */
     this.svg = this.createSVGElement('svg');
     this.svg.classList.add('netgraph');    
     this.svg.style.width = '100%';
@@ -19,14 +17,16 @@ VIZ.NetGraph = function(args) {
     args.parent.appendChild(this.svg);
     this.parent = args.parent;
     
-    this.g_networks = this.createSVGElement('g');
+    /** three separate layers, so that expanded networks are at the back,
+     *  then connection lines, and then other items (nodes, ensembles, and
+     *  collapsed networks) are drawn on top. */
+    this.g_networks = this.createSVGElement('g'); 
     this.svg.appendChild(this.g_networks);
     this.g_conns = this.createSVGElement('g');
     this.svg.appendChild(this.g_conns);
     this.g_items = this.createSVGElement('g');
     this.svg.appendChild(this.g_items);
     
-        
     /** connect to server */
     this.ws = new WebSocket('ws://localhost:8080/viz_component?id=' + args.id);
     this.ws.binaryType = "arraybuffer";
@@ -36,37 +36,47 @@ VIZ.NetGraph = function(args) {
     this.svg.addEventListener("resize", function() {self.on_resize();});
     window.addEventListener("resize", function() {self.on_resize();});
     
-    this.scale = 1.0;
-    this.offsetX = 0;
+    this.scale = 1.0;          // global scaling factor
+    this.offsetX = 0;          // global x,y pan offset 
     this.offsetY = 0;
-    this.svg_objects = {};
-    this.svg_conns = {};
-    this.collapsed_conns = {};
+
+    this.svg_objects = {};     // dict of all VIZ.NetGraphItems, by uid
+    this.svg_conns = {};       // dict of all VIZ.NetGraphConnections, by uid
+
+    /** Since connections may go to items that do not exist yet (since they
+     *  are inside a collapsed network), this dictionary keeps a list of
+     *  connections to be notified when a particular item appears.  The
+     *  key in the dictionary is the uid of the nonexistent item, and the
+     *  value is a list of VIZ.NetGraphConnections that should be notified
+     *  when that item appears. */
+    this.collapsed_conns = {}; 
     
+    /** dragging the background pans the full area by changing offsetX,Y */
     var self = this;
     interact(this.svg)
         .draggable({
             onmove: function(event) {
-                var w = self.get_scaled_width();
-                var h = self.get_scaled_height(); 
-                var dx = event.dx / w;
-                var dy = event.dy / h;
-                self.offsetX += dx;
-                self.offsetY += dy;
+                self.offsetX += event.dx / self.get_scaled_width();
+                self.offsetY += event.dy / self.get_scaled_height();
                 for (var key in self.svg_objects) {
                     self.svg_objects[key].redraw();
                 }    
             },
             onend: function(event) {
-                self.ws.send(JSON.stringify({act:"pan", x:self.offsetX,
-                                                        y:self.offsetY}));
+                /** let the server know what happened */
+                self.notify({act:"pan", x:self.offsetX, y:self.offsetY});
             }});
-    
+
+    /** scrollwheel on background zooms the full area by changing scale.
+     *  Note that offsetX,Y are also changed to zoom into a particular
+     *  point in the space */
     interact(this.svg)
         .on('wheel', function(event) {
             var x = (event.clientX / $(self.svg).width());
             var y = (event.clientY / $(self.svg).height());
-            var step_size = 1.1;
+
+            var step_size = 1.1; // size of zoom per wheel click
+
             var delta = event.wheelDeltaY || -event.deltaY
             var scale = delta > 0 ? step_size : 1.0 / step_size;
             
@@ -80,15 +90,15 @@ VIZ.NetGraph = function(args) {
             self.offsetY = self.offsetY / scale - dh * y / (h * scale);
                     
             self.scale = scale * self.scale;
-            for (var key in self.svg_objects) {
-                self.svg_objects[key].redraw();
-            }    
-            self.ws.send(JSON.stringify({act:"zoom", scale:self.scale, 
-                                         x:self.offsetX, y:self.offsetY}));
+
+            self.redraw();
+
+            /** let the server know what happened */
+            self.notify({act:"zoom", scale:self.scale, 
+                         x:self.offsetX, y:self.offsetY});
         });
-    
-    
 };
+
 
 /** Event handler for received WebSocket messages */
 VIZ.NetGraph.prototype.on_message = function(event) {
@@ -108,17 +118,29 @@ VIZ.NetGraph.prototype.on_message = function(event) {
     }
 };  
 
+
+/** report an event back to the server */
+VIZ.NetGraph.prototype.notify = function(info) {
+    this.ws.send(JSON.stringify(info));
+}
+
+
+/** pan the screen (and redraw accordingly) */
 VIZ.NetGraph.prototype.set_offset = function(x, y) {
     this.offsetX = x;
     this.offsetY = y;
     this.redraw();
 }
 
+
+/** zoom the screen (and redraw accordingly) */
 VIZ.NetGraph.prototype.set_scale = function(scale) {
     this.scale = scale;
     this.redraw();
 }
 
+
+/** redraw all elements */
 VIZ.NetGraph.prototype.redraw = function() {
     for (var key in this.svg_objects) {
         this.svg_objects[key].redraw();
@@ -126,37 +148,48 @@ VIZ.NetGraph.prototype.redraw = function() {
 }
 
 
+/** helper function for correctly creating SVG elements */
 VIZ.NetGraph.prototype.createSVGElement = function(tag) {
     return document.createElementNS("http://www.w3.org/2000/svg", tag);
 }
 
 
+/** Create a new NetGraphItem 
+ *  if an existing NetGraphConnection is looking for this item, it will be
+ *  notified */
 VIZ.NetGraph.prototype.create_object = function(info) {
     var item = new VIZ.NetGraphItem(this, info);
     this.svg_objects[info.uid] = item;    
     this.detect_collapsed_conns(item.uid);
 };
 
+
+/** create a new NetGraphConnection */
 VIZ.NetGraph.prototype.create_connection = function(info) {
     var conn = new VIZ.NetGraphConnection(this, info);
     this.svg_conns[info.uid] = conn;    
 };
 
+
+/** handler for resizing the full SVG */
 VIZ.NetGraph.prototype.on_resize = function(event) {
-    for (var key in this.svg_objects) {
-        var item = this.svg_objects[key];
-        item.set_position(item.pos[0], item.pos[1]);
-        item.set_size(item.size[0], item.size[1]);
-    }
+    this.redraw();
 };
 
+
+/** return the pixel width of the SVG times the current scale factor */
 VIZ.NetGraph.prototype.get_scaled_width = function() {
     return $(this.svg).width() * this.scale;
 }
+
+
+/** return the pixel height of the SVG times the current scale factor */
 VIZ.NetGraph.prototype.get_scaled_height = function() {
     return $(this.svg).height() * this.scale;
 }
 
+
+/** expand or collapse a network */
 VIZ.NetGraph.prototype.toggle_network = function(uid) {
     var item = this.svg_objects[uid];
     if (item.expanded) {
@@ -166,6 +199,11 @@ VIZ.NetGraph.prototype.toggle_network = function(uid) {
     }
 }
 
+
+/** register a NetGraphConnection with a target item that it is looking for
+ *  This is a NetGraphItem that does not exist yet, because it is inside a
+ *  collapsed network.  When it does appear, NetGraph.detect_collapsed will
+ *  handle notifying the NetGraphConnection. */
 VIZ.NetGraph.prototype.register_conn = function(conn, target) {
     if (this.collapsed_conns[target] === undefined) {
         this.collapsed_conns[target] = [conn];
@@ -173,12 +211,21 @@ VIZ.NetGraph.prototype.register_conn = function(conn, target) {
         this.collapsed_conns[target].push(conn);
     }
 }
+
+
+/** if a NetGraphConnection is looking for an item with a particular uid,
+ *  but that item does not exist yet (due to it being inside a collapsed
+ *  network), then it is added to the collapsed_conns dicutionary.  When
+ *  an item is create, this function is used to see if any NetGraphConnections
+ *  are waiting for it, and notifies them. */
 VIZ.NetGraph.prototype.detect_collapsed_conns = function(uid) {
     var conns = this.collapsed_conns[uid];
     if (conns != undefined) {
         delete this.collapsed_conns[uid];
         for (var i in conns) {
             var conn = conns[i];
+            /** make sure the NetGraphConnection hasn't been removed since
+             *  it started listening */
             if (!conn.removed) {
                 conn.set_pre(conn.find_pre());
                 conn.set_post(conn.find_post());
