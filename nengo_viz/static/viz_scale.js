1e167ea963624fc2fe77f674137ea4286474849b
Chris Chan <c88chan@CTN05.(none)>
2015-04-01 13:34:57 -0400

    functionized scaling

--- nengo_viz/static/viz_scale.js
+++ nengo_viz/static/viz_scale.js
@@ -10,46 +10,59 @@ VIZ.scale.events = function (){
 		var step_size = 1.1;
 		var wheel = event.wheelDelta / 120;
 		wheel = wheel < 0 ? step_size : 1.0 / step_size;
-
-		VIZ.scale.cumulative *= wheel;
-		console.log(VIZ.scale.cumulative)
-		//////////////////////////////////
-		var screen_px_x = $('#netgraph').width();
-
-		var cord_width = VIZ.pan.cposn.lr.x - VIZ.pan.cposn.ul.x; 
-
-		var scale_x = screen_px_x / cord_width;
-
-		var mouse_cord_x = (event.offsetX * cord_width / screen_px_x) + VIZ.pan.cposn.ul.x;
-
-		var post_cord_width = cord_width * wheel;
-
-		var post_scale_x = screen_px_x / post_cord_width;
-
-		var new_urx = (mouse_cord_x - (scale_x * mouse_cord_x - scale_x * VIZ.pan.cposn.ul.x) / post_scale_x); 
-		
-		////////////////////////////
-
-		var screen_px_y = $('#netgraph').height();
-
-		var cord_height = VIZ.pan.cposn.lr.y - VIZ.pan.cposn.ul.y; 
-
-		var scale_y = screen_px_y / cord_height; 
-
-		var mouse_cord_y = (event.offsetY * cord_height / screen_px_y) + VIZ.pan.cposn.ul.y;
-
-		var post_cord_height = cord_height * wheel;
-
-		var post_scale_y = screen_px_y / post_cord_height; 
-
-		var new_ury = (mouse_cord_y - (scale_y * mouse_cord_y - scale_y * VIZ.pan.cposn.ul.y) / post_scale_y);
-
-		////////////////////////////////////////////////////
-
-		VIZ.pan.cposn.ul.x = new_urx;
-		VIZ.pan.cposn.ul.y = new_ury;
-		VIZ.pan.cposn.lr.x = new_urx + post_cord_width;
-		VIZ.pan.cposn.lr.y = new_ury + post_cord_height;
-		VIZ.pan.redraw();
+		VIZ.scale.zoom(wheel, event);
 	});
+}
+
+VIZ.scale.zoom = function (wheel, event){
+
+	VIZ.scale.cumulative *= wheel;
+
+	if (event === undefined) {
+		var offsetX = 0;
+		var offsetY = 0;
+	}
+	else{
+		var offsetX = event.offsetX;
+		var offsetY = event.offsetY;
+	}
+	
+	////////////////////////////////// X scaling
+	var screen_px_x = $('#netgraph').width();
+
+	var cord_width = VIZ.pan.cposn.lr.x - VIZ.pan.cposn.ul.x; 
+
+	var scale_x = screen_px_x / cord_width;
+
+	var mouse_cord_x = (offsetX * cord_width / screen_px_x) + VIZ.pan.cposn.ul.x;
+
+	var post_cord_width = cord_width * wheel;
+
+	var post_scale_x = screen_px_x / post_cord_width;
+
+	var new_urx = (mouse_cord_x - (scale_x * mouse_cord_x - scale_x * VIZ.pan.cposn.ul.x) / post_scale_x); 
+	
+	//////////////////////////// Y scaling
+
+	var screen_px_y = $('#netgraph').height();
+
+	var cord_height = VIZ.pan.cposn.lr.y - VIZ.pan.cposn.ul.y; 
+
+	var scale_y = screen_px_y / cord_height; 
+
+	var mouse_cord_y = (offsetY * cord_height / screen_px_y) + VIZ.pan.cposn.ul.y;
+
+	var post_cord_height = cord_height * wheel;
+
+	var post_scale_y = screen_px_y / post_cord_height; 
+
+	var new_ury = (mouse_cord_y - (scale_y * mouse_cord_y - scale_y * VIZ.pan.cposn.ul.y) / post_scale_y);
+
+	////////////////////////////////////////////////////
+
+	VIZ.pan.cposn.ul.x = new_urx;
+	VIZ.pan.cposn.ul.y = new_ury;
+	VIZ.pan.cposn.lr.x = new_urx + post_cord_width;
+	VIZ.pan.cposn.lr.y = new_ury + post_cord_height;
+	VIZ.pan.redraw();
 }
