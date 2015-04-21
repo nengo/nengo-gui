645eb7e361435d6b38d06df62be753f0653af605
Chris Chan <c88chan@CTN05.(none)>
2015-04-21 12:00:58 -0400

    renamed the screen

--- nengo_viz/static/viz_scale.js
+++ nengo_viz/static/viz_scale.js
@@ -19,8 +19,8 @@ VIZ.scale.zoom = function (wheel, event){
 	VIZ.scale.cumulative *= wheel;
 
 	if (event === undefined) {
-		var offsetX = VIZ.pan.cposn.ul.x;
-		var offsetY = VIZ.pan.cposn.ul.y;
+		var offsetX = VIZ.Screen.ul.x;
+		var offsetY = VIZ.Screen.ul.y;
 	}
 	else{
 		var offsetX = event.offsetX;
@@ -30,40 +30,40 @@ VIZ.scale.zoom = function (wheel, event){
 	////////////////////////////////// X scaling
 	var screen_px_x = $('#netgraph').width();
 
-	var cord_width = VIZ.pan.cposn.lr.x - VIZ.pan.cposn.ul.x; 
+	var cord_width = VIZ.Screen.lr.x - VIZ.Screen.ul.x; 
 
 	var scale_x = screen_px_x / cord_width;
 
-	var mouse_cord_x = (offsetX * cord_width / screen_px_x) + VIZ.pan.cposn.ul.x;
+	var mouse_cord_x = (offsetX * cord_width / screen_px_x) + VIZ.Screen.ul.x;
 
 	var post_cord_width = cord_width * wheel;
 
 	var post_scale_x = screen_px_x / post_cord_width;
 
-	var new_urx = (mouse_cord_x - (scale_x * mouse_cord_x - scale_x * VIZ.pan.cposn.ul.x) / post_scale_x); 
+	var new_urx = (mouse_cord_x - (scale_x * mouse_cord_x - scale_x * VIZ.Screen.ul.x) / post_scale_x); 
 	
 	//////////////////////////// Y scaling
 
 	var screen_px_y = $('#netgraph').height();
 
-	var cord_height = VIZ.pan.cposn.lr.y - VIZ.pan.cposn.ul.y; 
+	var cord_height = VIZ.Screen.lr.y - VIZ.Screen.ul.y; 
 
 	var scale_y = screen_px_y / cord_height; 
 
-	var mouse_cord_y = (offsetY * cord_height / screen_px_y) + VIZ.pan.cposn.ul.y;
+	var mouse_cord_y = (offsetY * cord_height / screen_px_y) + VIZ.Screen.ul.y;
 
 	var post_cord_height = cord_height * wheel;
 
 	var post_scale_y = screen_px_y / post_cord_height; 
 
-	var new_ury = (mouse_cord_y - (scale_y * mouse_cord_y - scale_y * VIZ.pan.cposn.ul.y) / post_scale_y);
+	var new_ury = (mouse_cord_y - (scale_y * mouse_cord_y - scale_y * VIZ.Screen.ul.y) / post_scale_y);
 
 	////////////////////////////////////////////////////
 
-	VIZ.pan.cposn.ul.x = new_urx;
-	VIZ.pan.cposn.ul.y = new_ury;
-	VIZ.pan.cposn.lr.x = new_urx + post_cord_width;
-	VIZ.pan.cposn.lr.y = new_ury + post_cord_height;
+	VIZ.Screen.ul.x = new_urx;
+	VIZ.Screen.ul.y = new_ury;
+	VIZ.Screen.lr.x = new_urx + post_cord_width;
+	VIZ.Screen.lr.y = new_ury + post_cord_height;
 	VIZ.pan.redraw();
 	for (var i = 0; i < VIZ.Component.components.length; i++){
 		var w = VIZ.Component.components[i].width
