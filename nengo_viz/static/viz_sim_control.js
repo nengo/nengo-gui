38750296ac2829923e9b3925d66ce7023d8076d5
Sugandha Sharma <sugandhasharma_17@yahoo.in>
2015-05-19 13:00:32 -0400

    Added EOL to ascii websocket messages

--- nengo_viz/static/viz_sim_control.js
+++ nengo_viz/static/viz_sim_control.js
@@ -82,6 +82,7 @@ VIZ.SimControl.prototype.on_message = function(event) {
 
 VIZ.SimControl.prototype.set_status = function(status) {
     var icon;
+    status = status.trim();
     if (status === 'building') {
         icon = 'glyphicon-cog';
         this.start_rotating_cog();
@@ -92,6 +93,8 @@ VIZ.SimControl.prototype.set_status = function(status) {
     } else if (status == 'running') {
         icon = 'glyphicon-pause';
         this.stop_rotating_cog();
+    } else {
+        console.log(['unknown status', status]);
     }
     this.pause_button_icon.className = "glyphicon " + icon;
 }
