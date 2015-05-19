9cec5c63a88274d784d699f742eed1c1395194aa
Brent Komer <brent.komer@gmail.com>
2015-05-19 13:11:57 -0400

    starting on undo stack

--- nengo_viz/components/action.py
+++ nengo_viz/components/action.py
@@ -27,12 +27,10 @@ class Expand(Action):
         self.uid = uid
 
     def apply(self):
-        #self.net_graph.act_expand(self.uid)
-        self.net_graph.to_be_sent.append(dict(type='expand',uid=uid))
+        self.net_graph.act_expand(self.uid)
     
     def undo(self):
-        #self.net_graph.act_collapse(self.uid)
-        self.net_graph.to_be_sent.append(dict(type='collapse',uid=uid))
+        self.net_graph.act_collapse(self.uid)
 
 class Collapse(Action):
     def __init__(self, net_graph, uid):
@@ -40,12 +38,10 @@ class Collapse(Action):
         self.uid = uid
 
     def apply(self):
-        #self.net_graph.act_collapse(self.uid)
-        self.net_graph.to_be_sent.append(dict(type='collapse',uid=uid))
+        self.net_graph.act_collapse(self.uid)
     
     def undo(self):
-        #self.net_graph.act_expand(self.uid)
-        self.net_graph.to_be_sent.append(dict(type='expand',uid=uid))
+        self.net_graph.act_expand(self.uid)
 
 class Pan(Action):
     def __init__(self, net_graph, x, y):
@@ -58,13 +54,14 @@ class Pan(Action):
     def apply(self):
         self.old_x, self.old_y = self.net_graph.config[self.net_graph.viz.model].pos
         self.net_graph.act_pan(self.new_x, self.new_y)
-        self.net_graph.to_be_sent.append(dict(type='pan',pan=[self.new_x,self.new_y]))
 
     def undo(self):
+        #print("inside undo")
+        #print( self.old_x, self.old_y)
+        #print( self.new_x, self.new_y)
         self.new_x, self.new_y = self.net_graph.config[self.net_graph.viz.model].pos
         self.net_graph.act_pan(self.old_x, self.old_y)
-        self.net_graph.to_be_sent.append(dict(type='pan',pan=[self.old_x,self.old_y]))
-"""
+
 class Pos(Action):
     def __init__(self, net_graph, uid, x, y, width, height):
         self.net_graph = net_graph
@@ -72,4 +69,4 @@ class Pos(Action):
         self.new_y = y
         self.old_x = 0
         self.old_y = 0
-"""
+
