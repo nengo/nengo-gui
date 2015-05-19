45cafe0104f0676311d3cb30f08c60f3d1a74448
travis dewolf <travis.dewolf@gmail.com>
2015-05-19 14:40:51 -0400

    open file basic working

--- nengo_viz/static/viz_sim_control.js
+++ nengo_viz/static/viz_sim_control.js
@@ -70,7 +70,6 @@ VIZ.SimControl.prototype.on_message = function(event) {
             toolbar.open_modal(parsed_data['data']);
         } 
     }
-
     else {
         var data = new Float32Array(event.data);
         this.time = data[0];
