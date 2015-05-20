7f39aa925f53db9d2ba23778819cd4adbe1a6a6c
Trevor Bekolay <tbekolay@gmail.com>
2015-05-20 15:17:59 -0400

    Implemented response and tuning curves plots
    On the Ensemble info window. Using d3.js. Looks pretty sweet!
    Tried to make functions such that it'll be easy to add new plots
    in other info modals (including new plot types).

--- nengo_viz/disposable_js.py
+++ nengo_viz/disposable_js.py
@@ -3,6 +3,7 @@
 import json
 
 import nengo
+from nengo.utils.ensemble import response_curves, tuning_curves
 
 
 def infomodal(viz, obj, uid):
@@ -12,19 +13,64 @@ def infomodal(viz, obj, uid):
         raise NotImplementedError()
 
 
+class PlotInfo(object):
+    def __init__(self, title, plot="none"):
+        self.title = title
+        self.plot = plot
+        self.warnings = []
+        self.x = None
+        self.y = None
+
+    def to_dict(self):
+        x, y = self.x, self.y
+        if self.plot == "multiline":
+            assert self.x.shape[0] == self.y.shape[1]
+            x = self.x.tolist()
+            y = [yy.tolist() for yy in self.y]
+        return {
+            'plot': self.plot,
+            'title': self.title,
+            'warnings': self.warnings,
+            'x': x,
+            'y': y,
+        }
+
+
 def ensemble_infomodal(viz, ens, uid):
     params = [(attr, str(getattr(ens, attr))) for attr in (
         'n_neurons', 'dimensions', 'radius', 'encoders', 'intercepts',
         'max_rates', 'eval_points', 'n_eval_points', 'neuron_type',
         'noise', 'seed') if getattr(ens, attr) is not None]
 
-    siminfo = {}
+    if viz.sim is None:
+        plots = ("Simulation not yet running. "
+                 "Start a simulation to see plots.")
+    else:
+        plots = []
+        rc = PlotInfo("Response curves", plot="multiline")
+        rc.x, rc.y = response_curves(ens, viz.sim)
+        rc.y = rc.y.T
+        if ens.n_neurons > 200:
+            rc.warnings.append("Only showing the first 200 neurons.")
+            rc.y = rc.y[:200]
+        plots.append(rc.to_dict())
 
-    # info = {}
-    # siminfo = {}
+        tc = PlotInfo("Tuning curves")
+        if ens.dimensions == 1:
+            tc.plot = "multiline"
+            tc.x, tc.y = tuning_curves(ens, viz.sim)
+            tc.y = tc.y.T
+            if ens.n_neurons > 200:
+                tc.warnings.append("Only showing the first 200 neurons.")
+                tc.y = tc.y[:200]
+        else:
+            tc.warnings.append("Tuning curves only shown for "
+                               "one-dimensional ensembles.")
+        plots.append(tc.to_dict())
+
     js = ['VIZ.Modal.title("Details for \'%s\'");' % viz.viz.get_label(ens)]
     js.append('VIZ.Modal.footer("close");')
     js.append('VIZ.Modal.ensemble_body("%s", %s, %s);' % (
-        uid, json.dumps(params), json.dumps(siminfo)))
+        uid, json.dumps(params), json.dumps(plots)))
     js.append('VIZ.Modal.show();')
     return '\n'.join(js)
