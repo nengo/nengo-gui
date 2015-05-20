7f1e594d91cb13f3cf3741c0df462fe39af82d89
Trevor Bekolay <tbekolay@gmail.com>
2015-05-20 15:17:59 -0400

    Implemented response and tuning curves plots
    On the Ensemble info window. Using d3.js. Looks pretty sweet!
    Tried to make functions such that it'll be easy to add new plots
    in other info modals (including new plot types).

--- nengo_viz/static/viz_modal.js
+++ nengo_viz/static/viz_modal.js
@@ -27,36 +27,86 @@ VIZ.Modal.footer = function(type){
     }
 }
 
-VIZ.Modal.ensemble_body = function(uid, params, plots, conninfo) {
-    var tabs = VIZ.Modal.info_body([{id: 'params', title: 'Parameters'},
-                                    {id: 'plots', title: 'Plots'},
-                                    {id: 'connections', title: 'Connections'}]);
-    VIZ.Modal.render_params(tabs.params, params, VIZ.tooltips.ens);
-    VIZ.Modal.render_plots(tabs.plots, plots);
-    VIZ.Modal.render_connections(tabs.connections, uid, conninfo);
-}
-
-VIZ.Modal.node_body = function(uid, params, plots, conninfo) {
-    var tabs = VIZ.Modal.info_body([{id: 'params', title: 'Parameters'},
-                                    {id: 'plots', title: 'Plots'},
-                                    {id: 'connections', title: 'Connections'}]);
-    VIZ.Modal.render_params(tabs.params, params, VIZ.tooltips.node);
-    VIZ.Modal.render_plots(tabs.plots, plots);
-    VIZ.Modal.render_connections(tabs.connections, uid, conninfo);
-}
-
-VIZ.Modal.net_body = function(uid, stats, conninfo) {
-    var tabs = VIZ.Modal.info_body([{id: 'stats', title: 'Statistics'},
-                                    {id: 'connections', title: 'Connections'}]);
-    VIZ.Modal.render_stats(tabs.stats, stats);
-    VIZ.Modal.render_connections(tabs.connections, uid, conninfo);
-}
-
 /**
- * Sets up the tabs for Info modals.
+ * Static multiline plot with shared x-axis
+ *
+ * @param {String} selector - Where the svg will be added
+ * @param {Array of Float} x - The shared x-axis
+ * @param {Array of Array of Float} ys - The y data for each line
  */
-VIZ.Modal.info_body = function(tabinfo) {
-    var tabdivs = {}
+function multiline_plot(selector, x, ys) {
+    var m = {left: 40, top: 0, right: 0, bottom: 30};
+    var w = 500 - m.left - m.right;
+    var h = 220 - m.bottom - m.top;
+
+    var scale_x = d3.scale.linear()
+        .domain([x[0], x[x.length-1]])
+        .range([m.left, w - m.right]);
+    var scale_y = d3.scale.linear()
+        .domain([0, d3.max(ys, function(y){ return d3.max(y); })])
+        .range([h+m.top, 0]);
+
+    // Add an SVG element with the desired dimensions and margin.
+    var graph = d3.select(selector).append("svg")
+	.attr("width", w + m.left + m.right)
+	.attr("height", h + m.bottom + m.top);
+
+    var xAxis = d3.svg.axis()
+        .scale(scale_x)
+        .orient("bottom")
+        .ticks(9);
+    graph.append("g")
+	.attr("class", "axis axis_x unselectable")
+	.attr("transform", "translate(0," + (h+m.top)  + ")")
+	.call(xAxis);
+
+    var yAxisLeft = d3.svg.axis()
+        .scale(scale_y)
+        .orient("left");
+    graph.append("g")
+	.attr("class", "axis axis_y unselectable")
+	.attr("transform", "translate(" + m.left + ",0)")
+	.call(yAxisLeft);
+
+    var colors = VIZ.make_colors(ys.length);
+
+    var line = d3.svg.line()
+	.x(function(d, i) { return scale_x(x[i]); })
+	.y(function(d) { return scale_y(d); })
+
+    graph.selectAll("path")
+        .data(ys)
+      .enter()
+        .append("path")
+        .attr("d", line)
+        .attr("class", "line")
+        .style("stroke", function(d, i) { return colors[i]; });
+}
+
+function render_plot($parent, plotinfo) {
+    $parent.append("<h4>" + plotinfo.title + "</h4>")
+
+    if (plotinfo.warnings.length > 0) {
+        var $warn = $('<div class="alert alert-warning" role="alert"/>')
+            .appendTo($parent);
+
+        for (var i = 0; i < plotinfo.warnings.length; i++) {
+            var $p = $('<p/>').appendTo($warn);
+            $p.append('<span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span>');
+            $p.append('<span class="sr-only">Warning:</span>');
+            $p.append(document.createTextNode(plotinfo.warnings[i]));
+        }
+    }
+
+    if (plotinfo.plot === 'multiline') {
+        multiline_plot($parent.get(0), plotinfo.x, plotinfo.y);
+    } else if (plotinfo.plot !== 'none') {
+        console.warn("Plot type " + plotinfo.plot +
+                     " not understood, or not implemented yet.");
+    }
+}
+
+VIZ.Modal.ensemble_body = function(uid, params, plots) {
     var $body = $('.modal-body').first();
     $body.empty();
 
@@ -141,6 +191,22 @@ VIZ.Modal.render_plots = function($parent, plots) {
  */
 VIZ.Modal.render_plot = function($parent, plotinfo) {
     $parent.append("<h4>" + plotinfo.title + "</h4>")
+    // Plots
+    var $plots = $('<div class="tab-pane" id="plots"/>').appendTo($content);
+
+    // This indicates an error (usually no sim running)
+    if (typeof plots === 'string') {
+        var $err = $('<div class="alert alert-danger" role="alert"/>')
+            .appendTo($plots);
+        $err.append('<span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span>');
+        $err.append('<span class="sr-only">Error:</span>');
+        $err.append(document.createTextNode(siminfo.errors[i]));
+    }
+    else {
+        for (var i=0; i < plots.length; i++) {
+            render_plot($plots, plots[i]);
+        }
+    }
 
     if (plotinfo.warnings.length > 0) {
         var $warn = $('<div class="alert alert-warning" role="alert"/>')
