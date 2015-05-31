26dea79a7047b43f4203dd600f30cf7b054253ea
Terry Stewart <tcstewar@uwaterloo.ca>
2015-05-31 01:17:21 -0400

    Made move_to_front function and handled children

--- nengo_viz/static/viz_netgraph_item.js
+++ nengo_viz/static/viz_netgraph_item.js
@@ -98,9 +98,7 @@ VIZ.NetGraphItem = function(ng, info) {
         .draggable({
             onstart: function () {
                 self.menu.hide_any();
-                if (!self.expanded) {
-                    self.g.parentNode.appendChild(self.g); // move to front
-                }
+                self.move_to_front();
             },
             onmove: function(event) {
                 var w = ng.get_scaled_width();
@@ -262,6 +260,14 @@ VIZ.NetGraphItem = function(ng, info) {
 VIZ.NetGraphItem.prototype.set_label = function(label) {
     this.label.innerHTML = label;
 }
+
+VIZ.NetGraphItem.prototype.move_to_front = function() {
+    this.g.parentNode.appendChild(this.g);
+
+    for (var item in this.children) {
+        this.children[item].move_to_front();
+    }
+};
 
 VIZ.NetGraphItem.prototype.generate_menu = function () {
     var self = this;
