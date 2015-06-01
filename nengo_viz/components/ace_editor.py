6e403ec685ff84dab260b198e51bad9e2a437301
Chris Chan <c88chan@CTN05.(none)>
2015-06-01 13:25:28 -0400

    working towards getting config working

--- nengo_viz/components/ace_editor.py
+++ nengo_viz/components/ace_editor.py
@@ -8,11 +8,11 @@ class AceEditor(Component):
     def __init__(self, viz, config, uid):
         super(AceEditor, self).__init__(viz, config, uid)
         self.viz = viz
-        self.code = 'hello world'
+        self.current_code = 'hello world'
         self.uid = uid
 
     def javascript(self):
-        return 'new VIZ.Ace("%s", "%s")' % (self.code, self.uid) ##feed VIZ.Ace a single string representing all the code for the model
+        return 'new VIZ.Ace("%s", "%s")' % (self.current_code, self.uid) ##feed VIZ.Ace a single string representing all the code for the model
 
     def message(self, msg):
         print('msg received');
