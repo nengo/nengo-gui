bd54727cdca4ddcb2011a236a123471c6c9a12d1
Trevor Bekolay <tbekolay@gmail.com>
2015-06-04 17:50:11 -0400

    Release version 0.1.0

--- nengo_gui/version.py
+++ nengo_gui/version.py
@@ -1,2 +1,14 @@
-version_info = (0, 0, 1)  # (major, minor, patch)
-version = '.'.join(str(v) for v in version_info)
+"""Nengo GUI version information.
+
+We use semantic versioning (see http://semver.org/).
+Additionally, '-dev' will be added to the version unless the code base
+represents a release version. Commits for which the version doesn't have
+'-dev' should be git tagged with the version.
+"""
+
+name = "nengo_gui"
+version_info = (0, 1, 0)  # (major, minor, patch)
+dev = False
+
+version = "{v}{dev}".format(v='.'.join(str(v) for v in version_info),
+                            dev='-dev' if dev else '')
