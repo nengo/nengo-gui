7ffd39d228ed8896397de2ce5d996f7a27776296
Terry Stewart <tcstewar@uwaterloo.ca>
2015-07-06 18:19:51 -0400

    Removed from __future__ import print_function
    Having this specification in the code meant that all user scripts would
    also have to stick with the Python 3 syntax!

--- nengo_gui/ipython.py
+++ nengo_gui/ipython.py
@@ -1,5 +1,3 @@
-from __future__ import print_function
-
 import atexit
 import socket
 import threading
--- nengo_gui/swi.py
+++ nengo_gui/swi.py
@@ -1,5 +1,3 @@
-from __future__ import print_function
-
 """
 Simple Web Interface
 Author: Terry Stewart terry.stewart@gmail.com http://terrystewart.ca
