dbc98ad61561208ac222404512b0b5253ad6ec6d
Terry Stewart <tcstewar@uwaterloo.ca>
2015-07-07 17:58:57 -0400

    A bunch of tutorials for complete novices to nengo

---
+++ nengo_gui/examples/tutorial/05-computing.py
@@ -0,0 +1,36 @@
+# Tutorial 5: Computing a function
+
+# Whenever we make a Connection between groups of neurons, we don't have to
+# just pass the information from one group of neurons to the next.  Instead,
+# we can also modify that information.  We do this by specifying a function,
+# and Nengo will connect the individual neurons to best approximate that
+# function.
+
+# In the example here, we are computing the square of the value.  So for an
+# input of -1 it should output 1, for 0 it should output 0, and for 1 it should
+# output 1.
+
+# You can change the function by adjusting the computations done in the
+# part of the code labelled "compute_this".  This can be any arbitrary Python
+# function.  For example, try computing the negative of x ("return -x").  Try
+# the absolute value ("return abs(x)").  You can also try more complex
+# functions like "return 1 if x > 0 else 0".
+
+import nengo
+
+model = nengo.Network()
+with model:
+    stim = nengo.Node(0)
+    a = nengo.Ensemble(n_neurons=50, dimensions=1)
+    nengo.Connection(stim, a)
+    
+    b = nengo.Ensemble(n_neurons=50, dimensions=1)
+    
+    def compute_this(x):
+        return x * x
+    
+    nengo.Connection(a, b, synapse=0.01, function=compute_this)
+    
+    
+    import time
+    slowdown = nengo.Node(lambda t: time.sleep(0.001)) 
---
+++ nengo_gui/examples/tutorial/11-memory.py
@@ -0,0 +1,37 @@
+# Tutorial 11: Memory
+
+# Nengo models can also store information over time.  To do this, we simply
+# make a Connection from an Ensemble back to itself.  That is, we form a
+# Connection that will feed a value into an Ensemble that is the same value
+# that the Ensemble is currently representing.  This means we can store data
+# over time.
+
+# To use such a system, connect into it with another Ensemble.  If that input
+# is zero, then the stored value should stay the same as it currently is.
+# If the input is positive, the stored value should increase.  If the input is
+# negative, it should decrease.
+
+# Notice that the input Connection has "transform=0.1".  That is to control
+# how strongly the input affects the stored value.  If you make the transform
+# larger, it will change more quickly.
+
+# Also notice that the recurrent Connection from b back to itself has
+# synapse=0.1.  This longer time constant makes the memory more stable, and
+# is also commonly found in the real brain for recurrent connections.
+
+# Mathematical Note: In the case where the input transform is exactly equal to
+# the recurrent synapse (as it is here), it turns out that the resulting system
+# should compute the mathematical integral of the input.
+
+import nengo
+
+model = nengo.Network()
+with model:
+    stim_a = nengo.Node(0)
+    a = nengo.Ensemble(n_neurons=50, dimensions=1)
+    nengo.Connection(stim_a, a)
+    
+    b = nengo.Ensemble(n_neurons=50, dimensions=1)
+    nengo.Connection(b, b, synapse=0.1)
+    nengo.Connection(a, b, transform=0.1)
+    
