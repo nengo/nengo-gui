e92d729ff228416a904bccaaa301f5d7e848e408
Seanny123 <saubin@uwaterloo.ca>
2015-08-26 09:57:50 -0400

    oops

--- nengo_gui/nengo_gui/__init__.py
+++
@@ -1,5 +0,0 @@
-from .gui import GUI
-from .viz import Viz   # deprecated
-from .version import version as __version__
-from .namefinder import NameFinder
-from .main import main
--- nengo_gui/nengo_gui/examples/default.py
+++
@@ -1,7 +0,0 @@
-import nengo
-
-model = nengo.Network()
-with model:
-    stim = nengo.Node([0])
-    a = nengo.Ensemble(n_neurons=50, dimensions=1)
-    nengo.Connection(stim, a)
