9e9a7ef8d177e87340032ed58c9121d21318b0c1
Sean Aubin <saubin@uwaterloo.ca>
2015-10-04 09:48:04 -0400

    Fixed HTMLview docs and little typos

--- nengo_gui/components/htmlview.py
+++ nengo_gui/components/htmlview.py
@@ -8,6 +8,9 @@ from nengo_gui.components.component import Component
 
 
 class HTMLView(Component):
+    """Arbitrary HTML display taking input from a Node
+    See nengo_gui/examples/basics/html.py for example usage"""
+
     def __init__(self, obj):
         super(HTMLView, self).__init__()
         self.obj = obj
