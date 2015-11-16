89092b0eff3e3bf215a16fef57ac2a7b0af6387e
Sean Aubin <saubin@uwaterloo.ca>
2015-11-16 17:13:32 -0500

    delete useless fil

--- nengo_gui/components/tests/test_component.py
+++
@@ -1,8 +0,0 @@
-from nengo_gui.components.component import Component
-
-def test_javascript_config():
-    # test that the config is applying defaults properly
-    info = dict(uid=123, label="pants",
-                n_lines=4, synapse=0)
-    co = Component()
-    json = co.javascript_config(info)
