b1d1de16f13787baaf78a0f376714aed541c45eb
Terry Stewart <tcstewar@uwaterloo.ca>
2015-11-24 16:28:50 -0500

    Updated function space examples to use distributions

--- nengo_gui/examples/function_space/01-basic.py
+++ nengo_gui/examples/function_space/01-basic.py
@@ -2,39 +2,47 @@ import nengo
 import nengo.utils.function_space
 reload(nengo.utils.function_space)
 
+nengo.dists.Function = nengo.utils.function_space.Function
+nengo.FunctionSpace = nengo.utils.function_space.FunctionSpace
+
+
 import numpy as np
 
 
+domain = np.linspace(-1, 1, 200)
 
 # define your function
-def gaussian(points, mean, sd):
-    return np.exp(-(points-mean)**2/(2*sd**2))
+def gaussian(mag, mean, sd):
+    return mag * np.exp(-(domain-mean)**2/(2*sd**2))
+
 
 # build the function space
-fs = nengo.utils.function_space.FunctionSpace(gaussian,
-                   pts=np.linspace(-1, 1, 200),
-                   n_samples=1000, n_basis=10,
-                   mean=nengo.dists.Uniform(-1, 1),
-                   sd=nengo.dists.Uniform(0.1, 0.7))
-
+fs = nengo.FunctionSpace(nengo.dists.Function(gaussian,
+                                              mean=nengo.dists.Uniform(-1, 1),
+                                              sd=nengo.dists.Uniform(0.1, 0.7),
+                                              mag=1),
+                         n_basis=10)
 
 model = nengo.Network()
 with model:
     ens = nengo.Ensemble(n_neurons=500, dimensions=fs.n_basis)
-    fs.set_encoders(ens,
-                    mean=nengo.dists.Uniform(-1, 1),
-                    sd=0.05)
-    fs.set_eval_points(ens, n_eval_points=1000,
-                       mean=nengo.dists.Uniform(-1, 1),
-                       sd=nengo.dists.Uniform(0.1, 0.5))
+    ens.encoders = fs.project(nengo.dists.Function(gaussian,
+                mean=nengo.dists.Uniform(-1, 1),
+                sd=0.05,
+                mag=1))
 
-    stimulus = fs.make_stimulus_node()
+    ens.eval_points = fs.project(nengo.dists.Function(gaussian,
+                       mean=nengo.dists.Uniform(-1, 1),
+                       sd=nengo.dists.Uniform(0.1, 0.5),
+                       mag=1))
+
+    stimulus = fs.make_stimulus_node(gaussian, 3)
     nengo.Connection(stimulus, ens)
 
     stim_control = nengo.Node([1, 0, 0.2])
     nengo.Connection(stim_control, stimulus)
 
-    plot = fs.make_plot_node(lines=2, n_pts=50)
+    plot = fs.make_plot_node(domain=domain, lines=2)
 
     nengo.Connection(ens, plot[:fs.n_basis], synapse=0.1)
     nengo.Connection(stimulus, plot[fs.n_basis:], synapse=0.1)
