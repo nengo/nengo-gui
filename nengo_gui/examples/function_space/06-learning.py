276b09bda60281b06a4cb18a685dbbae64f9ebf1
travis dewolf <travis.dewolf@gmail.com>
2015-12-09 18:40:01 -0500

    learning functions attempt

--- nengo_gui/examples/function_space/06-learning.py
+++ nengo_gui/examples/function_space/06-learning.py
@@ -7,137 +7,107 @@ nengo.FunctionSpace = nengo.utils.function_space.FunctionSpace
 
 import numpy as np
 
-n_samples = 200
-domain = np.linspace(-1, 1, n_samples)
+domain_samples = 200
+domain = np.linspace(-1, 1, domain_samples)
 
 def gaussian(mag, mean, sd):
     return mag * np.exp(-(domain-mean)**2/(2*sd**2))
 
 # build the function space
-n_basis = 30
-gaussian_space =  nengo.dists.Function(gaussian, 
-                                       mean=nengo.dists.Uniform(-1, 1),
-                                       sd=nengo.dists.Uniform(0.01, 0.1),
-                                       mag=nengo.dists.Uniform(-1, 1)) 
-fs = nengo.FunctionSpace(gaussian_space, n_basis=n_basis)
+fs = nengo.FunctionSpace(nengo.dists.Function(gaussian,
+                                              mean=nengo.dists.Uniform(-1, 1),
+                                              sd=nengo.dists.Uniform(0.1, 0.7),
+                                              mag=1),
+                         n_basis=10)
+# for creating encoders and eval points
+def sample(mean_minmax, sd, mag):
+    return fs.project(nengo.dists.Function(gaussian,
+                      mean=nengo.dists.Uniform(-mean_minmax, mean_minmax),
+                      sd=sd, mag=mag))
 
 model = nengo.Network()
-model.config[nengo.Ensemble].neuron_type = nengo.Direct()
 with model:
-   
-    # ensemble that store weights over basis functions 
-    # for the compressed function representation
-    weights = nengo.Ensemble(n_neurons=1500, dimensions=fs.n_basis, 
-            radius=5, neuron_type=nengo.LIF())
+
+    ens = nengo.Ensemble(n_neurons=500, dimensions=fs.n_basis)
     # set encoders to be sampled from weights for common functions
-    weights.encoders = fs.project(gaussian_space)
+    ens.encoders = sample(1, 0.05, 1)
     # set eval points to be sampled from weights for common functions
-    weights.eval_points = fs.project(gaussian_space)
+    ens.eval_points = sample(1, nengo.dists.Uniform(0.1, 0.5), 1)
 
-    # input to the stimulus population, whose output projects the 
-    # compressed function representation weights into weights
-    # change stim_control for learning different functions in 
-    # the same set of decoders
-    stimulus = nengo.Ensemble(n_neurons=1000, dimensions=1, 
-            neuron_type=nengo.LIF())
-    # learning connection 
-    stim_conn = nengo.Connection(stimulus, weights, 
+    # the value that we would like the represented function to
+    # be at the current stimulus value (x)
+    target_val = nengo.Node(output=[0])
+
+    stimulus = nengo.Ensemble(n_neurons=500, dimensions=1)
+    stim_conn = nengo.Connection(stimulus, ens,
             function=lambda x: np.zeros(fs.n_basis),
-            learning_rule_type=nengo.PES(learning_rate=.0005))
-    # handy function plotting node to see the represented function 
-    plot = fs.make_plot_node(domain, lines=1, n_pts=100, 
-                             max_x=1, min_x=-1, max_y=2, min_y=-2)
-    nengo.Connection(weights, plot, synapse=0.1)
+            learning_rule_type=nengo.PES(learning_rate=.01))
 
-    # the x value we'd like to sample from the represented function
-    x = nengo.Node(output=lambda x: np.sin(x*2))
+    stim_control = nengo.Node([1])
+    nengo.Connection(stim_control, stimulus)
 
-    # in this case, learn two different Gaussians, 
-    # for different stim control values, which change 
-    # every so often (2 seconds at the moment)
-    def train_gaussians(t,x):
-        stim = 1 if int(t) % 2 == 0 else -1 
-        g = gaussian(1, stim/2.0, .05)
-        #g = stim/2.0 * np.linspace(-1, 1, n_samples)
-        index = int(x[0] * n_samples/2 + n_samples/2)
-        return g[index], stim
-    # the target f(x) value for the represented function
-    target_fx = nengo.Node(output=train_gaussians, size_in=1)
-    nengo.Connection(x, target_fx)
-    nengo.Connection(target_fx[1], stimulus)
+    plot = fs.make_plot_node(domain, lines=1, n_pts=50)
 
-    # this is for sampling from the represented function
-    product = nengo.networks.Product(n_neurons=1, dimensions=fs.n_basis)
+    nengo.Connection(ens, plot, synapse=0.1)
+
+    value = nengo.Node(output=np.sin)
+    x = nengo.Ensemble(n_neurons=500, dimensions=1)
+    nengo.Connection(value, x)
+
+    product = nengo.networks.Product(n_neurons=100, dimensions=fs.n_basis)
+
     sv_size = (fs.S/fs.scale)[:fs.n_basis]
+
     max_basis = np.max(fs.basis*fs.scale)
     # here we are getting the activation at point x from each basis function
-    # and passing that in to product.B, we then get the weights for each of 
+    # and passing that in to product.B, we then get the weights for each of
     # those basis functions and pass that in to product.A
-    # the weighted summation of these across all dimensions is the value of the 
+    # the weighted summation of these across all dimensions is the value of the
     # represented function at x
     for i in range(fs.n_basis):
         def basis_fn(x, i=i):
             # get a value in the range domain samples
-            # TODO: there should also be scaling for range 
-            # represented by the function
-            index = int(x[0]*n_samples/2 + n_samples/2)
-            if index > (n_samples - 1): index = n_samples - 1
+            # TODO: there should also be scaling for range represented by
+            # the function, right?
+            index = int(x[0]*domain_samples + domain_samples)
+            if index > (domain_samples - 1): index = domain_samples - 1
             if index < 0: index = 0
             return fs.basis[index][i]*fs.scale/max_basis
         nengo.Connection(x, product.B[i], function=basis_fn)
-        nengo.Connection(weights[i], product.A[i], transform=1.0/sv_size[i])
+        nengo.Connection(ens[i], product.A[i], transform=1.0/sv_size[i])
 
-    # the population representing f(x)
-    fx = nengo.Ensemble(n_neurons=100, dimensions=1, 
-            neuron_type=nengo.LIF())
-    nengo.Connection(product.output, fx, transform=[sv_size*max_basis])
 
-    # calculate the error at the sampled function value
-    error_fx = nengo.Ensemble(n_neurons=100, dimensions=1, 
-            neuron_type=nengo.LIF())
-    nengo.Connection(target_fx[0], error_fx, transform=-1)
-    nengo.Connection(fx, error_fx, transform=1)
+    total = nengo.Ensemble(50, 1)
+    nengo.Connection(product.output, total, transform=[sv_size*max_basis])
 
-    # project error into compressed function space
-    bar_x = np.linspace(0, 50, n_samples)
+    error = nengo.Ensemble(n_neurons=1, dimensions=1, neuron_type=nengo.Direct())
+
+    nengo.Connection(target_val, error)
+    nengo.Connection(total, error, transform=-1)
+
+    padder = nengo.Ensemble(n_neurons=1000, dimensions=2, neuron_type=nengo.Direct())
+    # so now we have the error and we need to project it back into
+    # compressed function representation space
+    # first we locate the error
     def pad_error(x):
-        error_fx = x[1]
+        error = x[1]
         x = x[0]
-        # put a representable Gaussian bump where the error is 
-        # in the function space with error amplitude
-        sd = 0.004
-        error_gauss = error_fx * np.exp(-(domain-x)**2/(2*sd**2))
+        # get a value in the range domain samples
+        index = int(x*domain_samples + domain_samples)
+        if index > (domain_samples - 1): index = domain_samples - 1
+        if index < 0: index = 0
+        padded_error = np.zeros(domain_samples)
+        padded_error[index] = -error
+        return padded_error
+    nengo.Connection(x, padder[0])
+    nengo.Connection(error, padder[1])
 
-        return error_gauss
-    error = nengo.Ensemble(n_neurons=1500, dimensions=2, )
-            # neuron_type=nengo.LIF())
-    error_filter = nengo.networks.EnsembleArray(100, fs.n_basis,
-            intercepts=nengo.dists.Uniform(.05, 1))
-    nengo.Connection(error, error_filter.input,
-            function=lambda x: fs.project(pad_error(x)))
-    nengo.Connection(error_filter.output, stim_conn.learning_rule)
-    nengo.Connection(x, error[0])
-    nengo.Connection(error_fx, error[1])
+    #filter_error = nengo.networks.EnsembleArray(n_neurons=50, n_ensembles=fs.n_basis,
+            #encoders=nengo.dists.Uniform(1,1), intercepts=nengo.dists.Uniform(.01, 1))
+    nengo.Connection(padder, stim_conn.learning_rule, function=lambda x: fs.project(pad_error(x)))
 
-    def pad_error_vis(t, x):
-        # # visualization code ----------------
-        scale = 20
-        y_bias = 50
-        line_width = 2
-        display_error_threshold = 0.001
-        bar_graph = '''<svg width="100%" height="100%" viewbox="0 0 100 100">'''
-        for ii in range(n_samples):
-            if abs(x[ii]) > display_error_threshold:
-                val = x[ii] * scale + y_bias 
-                bar_graph += '''<line x1="{0}" y1="51" x2="{0}" y2="{1}" stroke-width="{2}" style="stroke:black"/>'''.format(
-                        bar_x[ii] * line_width, val, line_width)
-        pad_error_vis._nengo_html_ = bar_graph
-        # end of visualization code ---------
-        
-    error_vis = nengo.Node(output=pad_error_vis, size_in=n_samples)
-    nengo.Connection(error, error_vis, 
-            function=pad_error)
-
+    display_error = nengo.Ensemble(n_neurons=1, dimensions=fs.n_basis, neuron_type=nengo.Direct())
+    nengo.Connection(padder, display_error, function=lambda x: fs.project(pad_error(x)))
+    #nengo.Connection(filter_error.output, stim_conn.learning_rule)
 
 sim = nengo.Simulator(model)
-sim.run(1)
