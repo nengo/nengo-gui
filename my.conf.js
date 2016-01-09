a8f72962bc89ab5c6db342a80a3860d350f70940
rsimmons1 <rees.polylabs@gmail.com>
2016-01-08 21:41:13 -0500

    Edited conf.js file, now Firefox dependent

--- my.conf.js
+++ my.conf.js
@@ -15,7 +15,7 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-      'tests/*.js'
+      '/nengo_gui/tests/*.js'
     ],
 
 
@@ -55,7 +55,7 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Chrome', 'Firefox'],
+    browsers: ['Firefox'],
 
 
     // Continuous Integration mode
