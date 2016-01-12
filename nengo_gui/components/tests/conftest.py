15700f91ed1e11d68f66990e2d9d39c779cef7e5
rsimmons1 <rees.polylabs@gmail.com>
2016-01-12 18:31:01 -0500

    Updated method of starting simulation, now running on saucelabs

--- nengo_gui/components/tests/conftest.py
+++ nengo_gui/components/tests/conftest.py
@@ -6,22 +6,23 @@ from selenium import webdriver
 import os
 import inspect
 # each function should get the same driver...
+
 @pytest.fixture(scope="module")
 def driver(request):
 	folder = os.path.dirname(inspect.getfile(nengo_gui))
 	example = os.path.join(folder,'examples/default.py')
-	# try:
-	# 	capabilities = {}
-	# 	username = os.environ["SAUCE_USERNAME"]
-	# 	access_key = os.environ["SAUCE_ACCESS_KEY"]
-	# 	capabilities["tunnel-identifier"] = os.environ["TRAVIS_JOB_NUMBER"]
-	# 	capabilities["browserName"] = 'chrome'
-	# 	capabilities["build"] = os.environ['TRAVIS_BUILD_NUMBER']
-	# 	hub_url = "%s:%s@localhost:4445" % (username, access_key)
-	# 	driver = webdriver.Remote(desired_capabilities=capabilities, command_executor="http://%s/wd/hub" % hub_url)
-	# except:                                                  #Setup Code for offline testing
-	driver = webdriver.Firefox()
+	try:
+		capabilities = {}
+		username = os.environ["SAUCE_USERNAME"]
+		access_key = os.environ["SAUCE_ACCESS_KEY"]
+		capabilities["tunnel-identifier"] = os.environ["TRAVIS_JOB_NUMBER"]
+		capabilities["browserName"] = 'chrome'
+		capabilities["build"] = os.environ['TRAVIS_BUILD_NUMBER']
+		hub_url = "%s:%s@localhost:4445" % (username, access_key)
+		driver = webdriver.Remote(desired_capabilities=capabilities, command_executor="http://%s/wd/hub" % hub_url)
+	except:                                                  #Setup Code for offline testing
+		driver = webdriver.Firefox()
 	driver.get('localhost:8080/?filename='+example)
-	time.sleep(6)
+	time.sleep(4)
 	return driver
 
--- nengo_gui/components/tests/test_selenium.py
+++ nengo_gui/components/tests/test_selenium.py
@@ -2,17 +2,40 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver import ActionChains
 import time
-
+import nengo_gui
 #This tests if selenium can find the Code editor and checks if it can take text from it. 
 def test_simple_selenium(driver):
 	#adding a connectionfile to
-	testString = """import nengo
-		model = nengo.Network()
-		with model:
-		    stim = nengo.Node([0])
-		    a = nengo.Ensemble(n_neurons=50, dimensions=1)
-		    nengo.Connection(stim, a)
-    """
+	testString = """
+# Nengo Network: Ensemble Array
+#
+# An ensemble array is a group of ensembles that each represent a part of the
+# overall signal.
+#
+# Ensemble arrays are similar to normal ensembles, but expose a slightly
+# different interface. Additionally, in an ensemble array, the components of
+# the overall signal are not related. As a result, network arrays cannot be
+# used to compute nonlinear functions that mix the dimensions they represent.
+
+import nengo
+import numpy as np
+
+model = nengo.Network()
+with model:
+    # Make an input node
+    sin = nengo.Node(lambda t: [np.cos(t), np.sin(t)])
+
+    # Make ensembles to connect
+    a = nengo.networks.EnsembleArray(n_neurons=100, n_ensembles=2)
+    b = nengo.Ensemble(n_neurons=100, dimensions=2)
+    c = nengo.networks.EnsembleArray(n_neurons=100, n_ensembles=2)
+
+    # Connect the model elements, just feedforward
+    nengo.Connection(sin, a.input)
+    nengo.Connection(a.output, b)
+    nengo.Connection(b, c.input)
+	"""
+	update_editor(driver,testString)
 	element = driver.find_element_by_xpath('//*[@class="ace_content"]')
 	text = element.get_attribute('textContent')
 	assert ''.join(text.split()) == ''.join(testString.split())
@@ -29,8 +52,15 @@ def test_graph_select(driver):
 		text = nodeLabel.get_attribute('textContent')
 		assert text == graphComponents[count]
 
+
+
 def test_node_labels(driver):
     nodes = driver.find_elements_by_xpath('//*[@class="graph"]')
     for n in nodes:
         n_text = n.get_attribute('textContent')
         assert n_text != ''
+
+def update_editor(driver,nengoCode):
+	nengoCode = nengoCode.replace("\n","\\n").replace("\r","\\r")
+	driver.execute_script("var editor = ace.edit('editor'); editor.setValue('"+nengoCode+"');")
+	time.sleep(1)
