a509774d0d5242e620c23b8fdd7e7f9743247b61
rsimmons1 <rees.polylabs@gmail.com>
2016-01-12 12:40:33 -0500

    Created Jasmine-Selenium Test

--- nengo_gui/tests/initialTest.js
+++ nengo_gui/tests/initialTest.js
@@ -1,7 +1,16 @@
 describe('Initial test', function(){
 	var d = "hello"
-
+	
 	it('should equal hello',function(){
-		expect(d).toBe('hello')
+		var webdriver = require('selenium-webdriver');
+
+		var driver = new webdriver.Builder().
+		   withCapabilities(webdriver.Capabilities.chrome()).
+		   build();
+		var testCode = "import nengo model = nengo.Network()with model: stim = nengo.Node([0]) a = nengo.Ensemble(n_neurons=50, dimensions=1) nengo.Connection(stim, a)"
+		driver.get('localhost:8080/?filename=nengo_gui/nengo_gui/examples/default.py');
+		driver.sleep(4000)
+		var textarea = $('#content');
+		expect(textarea.val().replace(/\s/g, '')).toBe(testCode.replace(/\s/g, ''))
 	});
 });
