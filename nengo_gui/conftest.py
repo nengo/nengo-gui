7c1dd1ed79f59c07308c4327ba906d62f35ef5bc
rsimmons1 <rees.polylabs@gmail.com>
2016-01-14 19:03:52 -0500

    Added mailing ability for test results

--- nengo_gui/conftest.py
+++ nengo_gui/conftest.py
@@ -4,6 +4,8 @@ import nengo_gui
 import pytest
 import os
 import inspect
+from fluentmail import FluentMail
+from fluentmail import TLS
 # each function should get the same driver...
 
 @pytest.fixture(scope="module")
@@ -16,5 +18,19 @@ def driver(request):
     def fin():
         print ("teardown selenium-webdriver")
         driver.close()
+        mail = FluentMail('smtp.gmail.com', 587, TLS)
+        print ("result being mailed too nengo.testing@gmail.com")
+        mail.credentials('nengo.testing@gmail.com', 'waterloou')\
+            .from_address('nengo.testing@gmail.com')\
+            .to('nengo.testing@gmail.com')\
+            .subject('Testing Picture')\
+            .body(u'', 'utf-8')\
+            .as_html()\
+            .attach('/home/travis/build/nengo/nengo_gui/screenshot.png', 'utf-8')\
+            .send()
     request.addfinalizer(fin)
     return driver
+
+
+
+
