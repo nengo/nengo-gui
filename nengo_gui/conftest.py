7a0838d68a720f4326a50d43cedf8143b30f74a9
rsimmons1 <rees.polylabs@gmail.com>
2016-01-18 17:04:20 -0500

    Attempting to get Saucelabs working

--- nengo_gui/conftest.py
+++ nengo_gui/conftest.py
@@ -17,7 +17,7 @@ def driver(request):
         capabilities["tunnel-identifier"] = os.environ["TRAVIS_JOB_NUMBER"]
         hub_url = "%s:%s@localhost:4445" % (username, access_key)
         driver = webdriver.Remote(desired_capabilities=capabilities, command_executor="http://%s/wd/hub" % hub_url)
-        driver.get('localhost:8080/?filename='+example)
+        driver.get('localhost:8080/')
         driver.maximize_window()
     except:
         driver = webdriver.Firefox()
