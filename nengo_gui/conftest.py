98bce9be7bb4700aa9a1480a89e37e86bb112a14
rsimmons1 <rees.polylabs@gmail.com>
2016-01-18 16:52:01 -0500

    Attempting to get Saucelabs working

--- nengo_gui/conftest.py
+++ nengo_gui/conftest.py
@@ -4,18 +4,25 @@ import nengo_gui
 import pytest
 import os
 import inspect
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 # each function should get the same driver...
 
 @pytest.fixture(scope="module")
 def driver(request):
     folder = os.path.dirname(inspect.getfile(nengo_gui))
     example = os.path.join(folder,'examples/default.py')
-    # try:
-    #     driver = webdriver.Chrome('/usr/lib/chromium/chromedriver')
-    # except:
-    driver = webdriver.Firefox()
-    driver.get('localhost:8080/?filename='+example)
-    driver.maximize_window()
+    try:
+        username = os.environ["SAUCE_USERNAME"]
+        access_key = os.environ["SAUCE_ACCESS_KEY"]
+        capabilities["tunnel-identifier"] = os.environ["TRAVIS_JOB_NUMBER"]
+        hub_url = "%s:%s@localhost:4445" % (username, access_key)
+        driver = webdriver.Remote(desired_capabilities=capabilities, command_executor="http://%s/wd/hub" % hub_url)
+        driver.get('localhost:8080/?filename='+example)
+        driver.maximize_window()
+    except:
+        driver = webdriver.Firefox()
+        driver.get('localhost:8080/?filename='+example)
+        driver.maximize_window()
     time.sleep(4)
     def fin():
         print ("teardown selenium-webdriver")
