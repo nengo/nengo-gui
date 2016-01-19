9c701145b73658763753fd389e2bae038c5ec909
rsimmons1 <rees.polylabs@gmail.com>
2016-01-19 14:01:13 -0500

    Setting up Imgur screenshots

--- nengo_gui/components/tests/test_selenium.py
+++ nengo_gui/components/tests/test_selenium.py
@@ -6,6 +6,7 @@ import pytest
 from nengo_gui import conftest
 from nengo_gui import testing_tools as tt
 import os
+
 test_files = tt.folder_location('examples/basics')
 
 @pytest.mark.parametrize('test_file',test_files)
@@ -17,6 +18,9 @@ def test_basic_functionality(driver,test_file):
 	assert len(ens_elements) > 0
 	tt.mouse_scroll(driver,500)
 	time.sleep(2)
+
+	#captures pictures in stdout
+	
 
 	#Creates graph objects by right clicking on nodes and selecting from menu
 	actions = ActionChains(driver)
@@ -56,11 +60,21 @@ def test_basic_functionality(driver,test_file):
 	time.sleep(1.5)
 	tt.start_stop_sim(driver)
 	time.sleep(0.5)
-	driver.get_screenshot_as_file('test_result.png')
-	picture_output = open('test_result.png','r')
-	print "##########START-TAG"
-	print picture_output.read()
-	print "##########END-TAG"
-	os.remove('test_result.png')
+	if(os.environ['TRAVIS']):
+		import pyimgur
+		driver.get_screenshot_as_file('test_result.png')
+		client_id = 'ce3e3bc9c9f0af0'
+		client_secret = 'b033592e871bd14ac89d3e7356d8d96691713170'
+		im = pyimgur.Imgur(client_id,client_secret)
+
+		current_folder = os.getcwd()
+		PATH = os.path.join(current_folder, 'test_result.png')
+		uploaded_image = im.upload_image(PATH, title="Uploaded with PyImgur")
+
+		print(uploaded_image.title)
+		print(uploaded_image.link)
+
+
+	
 
     
