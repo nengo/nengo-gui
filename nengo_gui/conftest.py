5f553ceaabd5cb0996ba337751c63601afa9c5c1
rsimmons1 <rees.polylabs@gmail.com>
2016-01-20 14:34:07 -0500

    Fixed Element not clickable error and updated error reporting

--- nengo_gui/conftest.py
+++ nengo_gui/conftest.py
@@ -1,25 +1,20 @@
 from selenium import webdriver
 import time 
 import pytest
-import os
-import inspect
 import nengo_gui
 # each function should get the same driver...
 
 @pytest.fixture(scope="module")
 def driver(request):
-    folder = os.path.dirname(inspect.getfile(nengo_gui))
-    example = os.path.join(folder,'examples/default.py')
     try:
         #driver = webdriver.Chrome('/usr/lib/chromium/chromedriver')
         driver = webdriver.Firefox()
     except:
         driver = webdriver.Firefox()
-    driver.get('localhost:8080/?filename='+example)
+    driver.get('localhost:8080/')
     driver.maximize_window()
     time.sleep(4)
     def fin():
-        print ("teardown selenium-webdriver")
         driver.close()
     request.addfinalizer(fin)
 
