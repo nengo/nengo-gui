147126946532cf09cf7d8e3955e3cbddeb17c2a0
Jan Gosmann <jan@hyper-world.de>
2016-02-05 17:36:07 -0500

    Implement upgrade to websocket.

--- nengo_gui/backend/server.py
+++ nengo_gui/backend/server.py
@@ -1,5 +1,48 @@
+import base64
 import BaseHTTPServer
+import hashlib
 
+
+WS_MAGIC = '258EAFA5-E914-47DA-95CA-C5AB0DC85B11'
 
 class HttpWsRequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
-    pass
+    def do_GET(self):
+        connection = self.headers.getheader('Connection', 'close').lower()
+        if connection == 'upgrade':
+            self.handle_upgrade()
+        else:
+            self.http_GET()
+
+    def http_GET(self):
+        pass
+
+    def handle_upgrade(self):
+        upgrade = self.headers.getheader('Upgrade').lower()
+        if upgrade == 'websocket':
+            self.upgrade_to_ws()
+        else:
+            pass  # FIXME produce error
+
+    def upgrade_to_ws(self):
+        response = '''HTTP/1.1 101 Switching Protocols
+Upgrade: websocket
+Connection: Upgrade
+Sec-WebSocket-Accept: {sec}
+'''
+
+
+        try:
+            # FIXME check
+            # A |Host| header field containing the server's authority.
+            key = self.headers['Sec-WebSocket-Key']
+            assert len(base64.b64decode(key)) == 16
+            assert self.headers['Sec-WebSocket-Version'] == '13'
+        except KeyError, AssertionError:
+            pass # FIXME Bad request
+
+        sec = base64.b64encode(hashlib.sha1(
+            key.encode('ascii') + WS_MAGIC).digest()).decode('ascii')
+
+        self.wfile.write(response.format(sec=sec))
+
+        # TODO handle communication
--- nengo_gui/backend/tests/test_server.py
+++ nengo_gui/backend/tests/test_server.py
@@ -1,14 +1,60 @@
-from StringIO import StringIO
+import re
+try:
+    from io import BytesIO
+except ImportError:  # Python 2.7
+    from BytesIO import BytesIO
 
-from nengo_gui.backend import server
+from nengo_gui import server
+
+
+class TwoWayStringIO(object):
+    def __init__(self, readable_data):
+        self.readable_data = readable_data
+        self.read_buffer = BytesIO(self.readable_data)
+        self.write_buffer = BytesIO()
+        self.written_data = None
+        self.closed = False
+
+    def read(self, *args, **kwargs):
+        return self.read_buffer.read(*args, **kwargs)
+
+    def readline(self, *args, **kwargs):
+        return self.read_buffer.readline(*args, **kwargs)
+
+    def write(self, *args, **kwargs):
+        return self.write_buffer.write(*args, **kwargs)
+
+    def close(self):
+        self.closed = True
+        self.written_data = self.write_buffer.getvalue()
+        self.read_buffer.close()
+
+    def flush(self):
+        self.write_buffer.flush()
+
+
+class ServerMock(object):
+    server_name = 'localhost'
+    server_port = 80
+
+    def create_websocket(self, socket):
+        return server.WebSocket(socket)
 
 
 class SocketMock(object):
     def __init__(self, request_data):
-        self.request_data = request_data
+        self.request_data = request_data.encode('utf-8')
+        self.file_io = TwoWayStringIO(self.request_data)
 
     def makefile(self, mode='r', bufsize=1024):
-        return StringIO(self.request_data)
+        return self.file_io
+
+    def send(self, data):
+        self.file_io.write(data)
+        return len(data)
+
+    def setblocking(self, value):
+        pass
 
 
 class TestHttpWsRequestHandler(object):
@@ -24,11 +70,120 @@ User-Agent: nengo_gui
                 server.HttpWsRequestHandler.__init__(
                     self, request, client_address, srv)
 
-            def do_GET(self):
+            def http_GET(self):
                 self.method_called = True
                 assert self.path == '/'
-                assert self.headers.getheader('host', 'localhost')
-                assert self.headers.getheader('user-agent', 'nengo_gui')
+                assert self.headers.get('host').lower() == 'localhost'
+                assert self.headers.get('user-agent').lower() == 'nengo_gui'
 
-        handler = HandlerClass(request, 'localhost', 'localhost')
+        handler = HandlerClass(request, 'localhost', ServerMock())
         assert handler.method_called
+
+    def test_upgrade_to_websocket(self):
+        request = SocketMock('''GET /resource_name HTTP/1.1
+Upgrade: websocket
+Connection: Upgrade
+Origin: http://localhost:80
+Host: localhost:80
+Sec-WebSocket-Key: AQIDBAUGBwgJCgsMDQ4PEC==
+Sec-WebSocket-Version: 13
+''')
+
+        class HandlerClass(server.HttpWsRequestHandler):
+            def ws_default(self):
+                pass
+
+        handler = HandlerClass(request, 'localhost', ServerMock())
+        # Ignore errors in decoding because binary WebSocket data might follow
+        # response.
+        response = request.file_io.written_data.decode(
+            'utf-8', errors='ignore')
+
+        assert re.match(r'^HTTP\/\d+\.\d+\s+101.*$', response, re.M)
+        assert re.search(r'^Upgrade:\s+.*websocket.*$', response, re.M | re.I)
+        assert re.search(r'^Connection:\s+.*upgrade.*$', response, re.M | re.I)
+        assert re.search(
+            r'^Sec-WebSocket-Accept:\s+OfS0wDaT5NoxF2gqm7Zj2YtetzM=\s+$',
+            response, re.M)
+
+    def test_bad_upgrade_to_websocket(self):
+        request = SocketMock('''GET /resource_name HTTP/1.1
+Upgrade: websocket
+Connection: Upgrade
+Origin: http://localhost:80
+Host: localhost:80
+Sec-WebSocket-Key: null
+Sec-WebSocket-Version: 13
+''')
+
+        handler = server.HttpWsRequestHandler(
+            request, 'localhost', ServerMock())
+        response = request.file_io.written_data.decode('utf-8')
+
+        assert re.match(r'^HTTP\/\d+\.\d+\s+400.*$', response, re.M)
+
+    def test_parsing_resource_get(self):
+        request = SocketMock('GET /res/file?p1=1&p2=2&p1=0 HTTP/1.1\r\n')
+
+        class HandlerClass(server.HttpWsRequestHandler):
+            def http_default(self):
+                pass
+
+        handler = HandlerClass(request, 'localhost', ServerMock())
+        assert handler.resource == '/res/file'
+        assert handler.query == {'p1': ['1', '0'], 'p2': ['2']}
+        assert handler.db == {'p1': '1', 'p2': '2'}
+
+    def test_parsing_resource_post(self):
+        content = 'p2=3&p3=0'
+        request = SocketMock('''POST /res/file?p1=1&p2=2&p1=0 HTTP/1.1
+Content-Type:application/x-www-form-urlencoded; charset=UTF-8
+Content-Length: {len}
+
+{content}
+'''.format(len=len(content), content=content))
+
+        class HandlerClass(server.HttpWsRequestHandler):
+            def http_default(self):
+                pass
+
+        handler = HandlerClass(request, 'localhost', ServerMock())
+        assert handler.resource == '/res/file'
+        assert handler.query == {'p1': ['1', '0'], 'p2': ['2']}
+        assert handler.db == {'p1': '1', 'p2': '3', 'p3': '0'}
+
+    def test_http_dispatch(self):
+        request = SocketMock('GET /file HTTP/1.1\r\n')
+
+        class HandlerClass(server.HttpWsRequestHandler):
+            called = False
+            http_commands = {'/file': 'cmd'}
+
+            def cmd(self):
+                self.called = True
+
+        handler = HandlerClass(request, 'localhost', ServerMock())
+        assert handler.called
+
+    def test_ws_dispatch(self):
+        request = SocketMock('''GET /ws HTTP/1.1
+Upgrade: websocket
+Connection: Upgrade
+Origin: http://localhost:80
+Host: localhost:80
+Sec-WebSocket-Key: AQIDBAUGBwgJCgsMDQ4PEC==
+Sec-WebSocket-Version: 13
+''')
+
+        class HandlerClass(server.HttpWsRequestHandler):
+            called = False
+            ws_commands = {'/ws': 'cmd'}
+            server = ServerMock()
+
+            def cmd(self):
+                self.called = True
+
+        handler = HandlerClass(request, 'localhost', ServerMock())
+        assert handler.called
+        assert handler.ws.state in [
+            server.WebSocket.ST_CLOSING, server.WebSocket.ST_CLOSED]
