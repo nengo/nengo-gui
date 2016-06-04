9c0e2f4259bb411f935a27566da829e13d70cb11
Terry Stewart <tcstewar@uwaterloo.ca>
2016-06-04 17:31:04 -0400

    Added timing to help tests pass

--- nengo_gui/tests/test_basic_functionality.py
+++ nengo_gui/tests/test_basic_functionality.py
@@ -17,6 +17,7 @@ def test_basic_functionality(driver, test_file):
 		#Test page response by clicking the reset button and applying new code to ace-editor
 
 		tt.reset_page(driver)
+                time.sleep(0.5)
 		tt.update_editor(driver, test_file)
 		tt.mouse_scroll(driver,500)
 		ens_elements = driver.find_elements_by_xpath('//*[@class="ens"]')
