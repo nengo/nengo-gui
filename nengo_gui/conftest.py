03a59e0ee597f4a1e971bd0f8060259e8a5ebdc0
Jan Gosmann <jan@hyper-world.de>
2016-07-22 21:03:21 -0400

    Start nengo_gui server as fixture for tests.
    This allows to run all tests with a simple `py.test nengo_gui`
    without the need to start the server manually. It also automatically
    selects a free port allowing to run another nengo_gui server or other
    things on the default things in parallel (e.g. to work on an actual
    model while the tests run).

--- nengo_gui/conftest.py
+++ nengo_gui/conftest.py
@@ -1,27 +1,51 @@
 from __future__ import print_function
-import time
+
+import os.path
+import socket
+import threading
+
 import pytest
 from selenium import webdriver
 
+import nengo_gui
+from nengo_gui import guibackend
+from nengo_gui.gui import BaseGUI
 
-@pytest.fixture(scope="session")
-def driver(request):
 
+@pytest.yield_fixture(scope="session")
+def gui():
+    host, port = ('localhost', 0)
+    server_settings = guibackend.GuiServerSettings((host, port))
+    model_context = guibackend.ModelContext(
+        filename=os.path.join(nengo_gui.__path__[0], 'examples', 'default.py'))
+    gui = BaseGUI(model_context, server_settings)
+    server_thread = threading.Thread(target=gui.start)
+    server_thread.start()
+    port = gui.server.server_port
+
+    started = False
+    while server_thread.is_alive() and not started:
+        try:
+            s = socket.create_connection((host, port), 0.1)
+            started = True
+        except:
+            pass
+        else:
+            s.close()
+
+    yield gui
+
+    gui.server.shutdown()
+    gui.server.wait_for_shutdown(0.05)
+
+
+@pytest.yield_fixture(scope="session")
+def driver(gui):
     driver = webdriver.Firefox()
-    driver.get('localhost:8080/')
+    driver.get('localhost:{port}/'.format(port=gui.server.server_port))
     driver.maximize_window()
-    time.sleep(4)
 
-    def fin():
-        driver.close()
+    assert driver.title != "Problem loading page"
+    yield driver
 
-    request.addfinalizer(fin)
-    try:
-        assert driver.title != "Problem loading page"
-
-    except AssertionError:
-        print("ERROR: The 'nengo' server is not currently running. "
-              "Start the server before running tests.")
-        raise
-
-    return driver
+    driver.close()
