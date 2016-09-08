57861a2f4764ea35d8c13fee60da0852a82a8a50
travis dewolf <travis.dewolf@gmail.com>
2016-09-08 15:23:42 -0400

    Updated function space 03 example
    - flake8 fixup
    - added comments
    - reworked to use make_input instead of make_stimulus_node

--- nengo_gui/examples/function_space/03-attend.py
+++ nengo_gui/examples/function_space/03-attend.py
@@ -25,8 +25,10 @@ fs = nengo.FunctionSpace(
 
 model = nengo.Network()
 with model:
-    # create an ensemble to represent the weights over the basis functions
+    # an ensemble to represent the weights over the basis functions
     ens = nengo.Ensemble(n_neurons=500, dimensions=fs.n_basis)
+    # this time we're using separate distributions for the encoders
+    # and the evaluation points. TODO: why?
     ens.encoders = fs.project(
         nengo.dists.Function(gaussian,
                              mean=nengo.dists.Uniform(-1, 1),
@@ -38,6 +40,7 @@ with model:
                              mag=nengo.dists.Uniform(0, 1),
                              superimpose=4))
 
+    # this population will find the salient points in the input
     choice = nengo.Ensemble(n_neurons=1000, dimensions=fs.n_basis, radius=2)
     choice.encoders = fs.project(
         nengo.dists.Function(gaussian,
@@ -50,6 +53,7 @@ with model:
                              mag=nengo.dists.Uniform(-1, 1),
                              superimpose=4))
 
+    # number of different Gaussian stimuli, creating competing attention points
     n_stims = 3
     for ii in range(n_stims):
         stim = fs.make_input([0, 0, .2])
@@ -59,12 +63,19 @@ with model:
     plot = fs.make_plot_node(domain, lines=2)
     nengo.Connection(ens, plot[:fs.n_basis], synapse=0.1)
 
+    # the function for choosing the most salient stimulus
     def collapse(x):
+        # reconstruct the represented function from weights x
         pts = fs.reconstruct(x)
+        # find the max value of the represented function
         peak = np.argmax(pts)
+        # output a set of weights for a Gaussian centered at the peak
         data = fs.project(gaussian(mag=1, sd=0.2, mean=domain[peak])*2-1)
         return data
 
+    # project weights from all stimuli into choice
     nengo.Connection(ens, choice)
+    # in a recurrent connection, bias the representation towards the
+    # max value of the function represented in ens
     nengo.Connection(choice, choice, function=collapse)
     nengo.Connection(choice, plot[fs.n_basis:], synapse=0.1)
