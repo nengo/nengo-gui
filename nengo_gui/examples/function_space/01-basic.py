b8147d84d0ca2956964f52062185af9cb813f5ce
travis dewolf <travis.dewolf@gmail.com>
2016-09-08 14:07:00 -0400

    Updated function space 01 example
    - flake8 cleaning
    - added comments
    - reworked to use make_input function instead of make_stimulus_node

--- nengo_gui/examples/function_space/01-basic.py
+++ nengo_gui/examples/function_space/01-basic.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 import nengo
 import nengo.utils.function_space
 reload(nengo.utils.function_space)
@@ -5,47 +7,42 @@ reload(nengo.utils.function_space)
 nengo.dists.Function = nengo.utils.function_space.Function
 nengo.FunctionSpace = nengo.utils.function_space.FunctionSpace
 
-
-import numpy as np
-
-
 domain = np.linspace(-1, 1, 200)
 
-# define your function
+
+# define kinds of functions you'd like to represent
 def gaussian(mag, mean, sd):
     return mag * np.exp(-(domain-mean)**2/(2*sd**2))
 
+# build the function space, generate the basis functions
+fs = nengo.FunctionSpace(
+    nengo.dists.Function(
+        gaussian,
+        mean=nengo.dists.Uniform(-1, 1),
+        sd=nengo.dists.Uniform(0.1, 0.7),
+        mag=1),
+    n_basis=10)
 
-# build the function space
-fs = nengo.FunctionSpace(nengo.dists.Function(gaussian,
-                                              mean=nengo.dists.Uniform(-1, 1),
-                                              sd=nengo.dists.Uniform(0.1, 0.7),
-                                              mag=1),
-                         n_basis=10)
+# create a distribution for generating encoders and eval points
+ens_dist = nengo.dists.Function(
+    gaussian,
+    mean=nengo.dists.Uniform(-1, 1),
+    sd=0.05,
+    mag=1)
 
 model = nengo.Network()
 with model:
+    # create an ensemble to represent the weights over the basis functions
     ens = nengo.Ensemble(n_neurons=500, dimensions=fs.n_basis)
-    ens.encoders = fs.project(nengo.dists.Function(gaussian,
-                mean=nengo.dists.Uniform(-1, 1),
-                sd=0.05,
-                mag=1))
+    # set encoders and evaluation points to be in a range that gets used
+    ens.encoders = fs.project(ens_dist)
+    ens.eval_points = fs.project(ens_dist)
 
-    ens.eval_points = fs.project(nengo.dists.Function(gaussian,
-                       mean=nengo.dists.Uniform(-1, 1),
-                       sd=nengo.dists.Uniform(0.1, 0.5),
-                       mag=1))
+    # create a network for input
+    input_net = fs.make_input([1, 0, 0.2])
+    nengo.Connection(input_net.output, ens)
 
-    stimulus = fs.make_stimulus_node(gaussian, 3)
-    nengo.Connection(stimulus, ens)
-
-    stim_control = nengo.Node([1, 0, 0.2])
-    nengo.Connection(stim_control, stimulus)
-
+    # create a node to give a plot of the represented function
     plot = fs.make_plot_node(domain=domain, lines=2)
 
     nengo.Connection(ens, plot[:fs.n_basis], synapse=0.1)
-    nengo.Connection(stimulus, plot[fs.n_basis:], synapse=0.1)
-
-sim = nengo.Simulator(model)
-sim.run(1)
