9d709716211b6109adc3d6e84eab650afbfc3d3d
Trevor Bekolay <tbekolay@gmail.com>
2016-10-23 10:27:53 -0400

    Remove some commented out code

--- nengo_gui/static/nengo-debug.ts
+++ nengo_gui/static/nengo-debug.ts
@@ -14,71 +14,11 @@ import * as interact from "interact.js";
 import "./favicon.ico";
 import "./nengo.css";
 
-// import { Editor } from "./editor";
-// import { NetGraph } from "./netgraph/netgraph";
-// import { SideMenu } from "./side-menu";
 import { SimControl } from "./sim-control";
-// import { Toolbar } from "./toolbar";
 import { MockConnection } from "./websocket";
-
-// TODO: put all of this in an ajax call to Python. To get:
-// editor uid (uid)
-// netgraph uid
-// simcontrol config/args (simconfig) -- shownTime, uid, keptTime
-// filename
-
-// export class Nengo {
-//     control;
-//     editor;
-//     hotkeys;
-//     main;
-//     modal;
-//     netgraph;
-//     sidemenu;
-//     sim;
-//     toolbar;
-
-//     constructor(simargs, filename, editoruid, netgraphargs) {
-//         this.main = document.getElementById("main");
-//         this.control = document.getElementById("control");
-
-//         this.netgraph = new NetGraph(this.main, netgraphargs);
-//         this.editor = new Editor(editoruid, this.netgraph);
-//         this.sim = new SimControl(this.control, simargs, this.editor);
-//         this.sidemenu = new SideMenu(this.sim);
-//         this.toolbar = new Toolbar(filename, this.sim);
-
-//         this.modal = this.sim.modal;
-//         this.hotkeys = this.modal.hotkeys;
-
-//         document.title = filename;
-//     }
-// }
-
-// $(document).ready(() => {
-//     body = document.getElementById("body");
-//     body.removeChild(document.getElementById("loading-div"));
-//     %(main_components)s
-//     nengo = new Nengo.default(simargs, filename, editoruid, netgraphargs);
-//     %(components)s
-// }
-
-// Exposing components for server
-// import "expose?HTMLView!./components/htmlview";
-// import "expose?Image!./components/image";
-// import "expose?Pointer!./components/pointer";
-// import "expose?Raster!./components/raster";
-// import "expose?Slider!./components/slider";
-// import "expose?SpaSimilarity!./components/spa-similarity";
-// import "expose?Value!./components/value";
-// import "expose?XYValue!./components/xyvalue";
-// import "expose?utils!./utils";
 
 import { DebugView } from "./views/debug";
 import { SimControlView } from "./views/sim-control";
-// import { first } from "./views/views";
-
-// import * as utils from "./utils";
 
 export class NengoDebug {
     view: DebugView = new DebugView();
