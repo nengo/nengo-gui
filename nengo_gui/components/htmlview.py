04485a582b4d5d437f89e67812c2296f6c4f4f95
Eric Hunsberger <erichuns@gmail.com>
2016-11-23 13:00:52 -0500

    fixup! User can provide custom function for HTML display

--- nengo_gui/components/htmlview.py
+++ nengo_gui/components/htmlview.py
@@ -12,42 +12,44 @@ class HTMLView(Component):
 
     def __init__(self, obj):
         super(HTMLView, self).__init__()
-        self.obj = obj
         self.data = collections.deque()
 
-        self.static = not callable(obj.output._nengo_html_)
-        if not self.static:
-            assert obj.size_out > 0
-            self.html = obj.output._nengo_html_
+        self.obj = obj
+        self.obj_output = self.obj.output
+        self.callable_html = callable(obj.output._nengo_html_)
+        if self.callable_html:
+            assert self.obj.size_out > 0
+        else:
+            assert callable(self.obj_output)
 
     def attach(self, page, config, uid):
         super(HTMLView, self).attach(page, config, uid)
         self.label = page.get_label(self.obj)
 
     def add_nengo_objects(self, page):
-        if self.static:
-            self.obj_output = self.obj.output
-            self.obj.output = self.gather_data
-        else:
+        if self.callable_html:
             with page.model:
                 self.node = nengo.Node(
                     self.gather_data, size_in=self.obj.size_out)
                 self.conn = nengo.Connection(self.obj, self.node, synapse=None)
+        else:
+            self.obj.output = self.gather_data
 
     def remove_nengo_objects(self, page):
-        if self.static:
-            self.obj.output = self.obj_output
-        else:
+        if self.callable_html:
             page.model.connections.remove(self.conn)
             page.model.nodes.remove(self.node)
+        else:
+            self.obj.output = self.obj_output
 
     def gather_data(self, t, *x):
-        if self.static:
+        if self.callable_html:
+            value = None
+            html = self.obj_output._nengo_html_(t, *x)
+        else:
             value = self.obj_output(t, *x)
             html = self.obj_output._nengo_html_
-        else:
-            value = None
-            html = self.html(t, *x)
+
         self.data.append('%g %s' % (t, html))
         return value
 
