16d970b045b130c40ebe3f862a0d18723d7dbc18
Jan Gosmann <jgosmann@uwaterloo.ca>
2017-02-07 18:32:05 -0500

    Fix password protection in Python 3.

--- nengo_gui/password.py
+++ nengo_gui/password.py
@@ -9,17 +9,20 @@ import os
 
 
 def gensalt(size=16):
-    return binascii.hexlify(os.urandom(size))
+    return binascii.hexlify(os.urandom(size)).decode('ascii')
+
 
 def hashpw(password, salt, algorithm='sha1'):
     h = hashlib.new(algorithm)
-    h.update(password)
-    h.update(salt)
+    h.update(password.encode('utf-8'))
+    h.update(salt.encode('ascii'))
     return algorithm + ':' + salt + ':' + h.hexdigest()
+
 
 def checkpw(password, hashed):
     algorithm, salt, _ = hashed.split(':')
     return hashpw(password, salt, algorithm) == hashed
+
 
 def prompt_pw():
     while True:
