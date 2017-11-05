538508b6c58ea337805dd3bac98612dff536b2d6
Jan Gosmann <jgosmann@uwaterloo.ca>
2017-11-05 16:33:46 -0500

    TRAVIS TEST

--- nengo_gui/tests/test_basic_functionality.py
+++ nengo_gui/tests/test_basic_functionality.py
@@ -18,11 +18,7 @@ def test_basic_functionality(driver, test_file):
     try:
         # Test page response by clicking the reset button and applying
         # new code to ace-editor
-        if('TRAVIS' in os.environ):
-            tt.imgur_screenshot(driver)
         tt.reset_page(driver)
-        if('TRAVIS' in os.environ):
-            tt.imgur_screenshot(driver)
         time.sleep(2.)
         if('TRAVIS' in os.environ):
             tt.imgur_screenshot(driver)
