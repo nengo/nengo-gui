f81286067dc28ad114dbdf7f5133c4c11fa36109
Terry Stewart <tcstewar@uwaterloo.ca>
2017-12-02 23:53:16 -0500

    Give access to current nengo_gui.Page

--- nengo_gui/page.py
+++ nengo_gui/page.py
@@ -211,6 +211,7 @@ class Page(object):
         code_locals = {}
         code_locals['nengo_gui'] = nengo_gui
         code_locals['__file__'] = self.filename
+        code_locals['__page__'] = self
 
         self.code = code
         self.error = None
